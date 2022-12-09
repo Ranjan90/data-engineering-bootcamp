f5b6975a359091e02009174ab063a59fe9e35372
Ranjan90 <ranju12390@gmail.com>
2022-12-09 23:40:41 +0530

    added assignment solution

--- Python/Assisgnment1/solution.py
+++ Python/Assisgnment1/solution.py
@@ -1,3 +1,15 @@
 print("welcome to Big data boot camp ")
 
-list1=[]
+list1=[10,20,30,56,34]
+list1.append(67)
+print(list1)
+
+print(10/3)
+print(10//3)
+c="iNeuron"
+print(c*4)
+
+print(1 or 0)
+print(True and False and True)
+
+print(1 or 0 or 0)
